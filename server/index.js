7d9606cd370fbe6ee0f62f36602f4b242e471688
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-10-14 17:06:10 -0400

    Finished basic setup

--- server/index.js
+++ server/index.js
@@ -1,21 +1,32 @@
-import express, { urlencoded } from 'express';
+import express, { json, urlencoded } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import routes from './routes/index.js';
 
 const app = express();
 
+// Security measure for targeted attacks
+app.disable('x-powered-by')
+
 // middleware
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
 app.use(morgan('dev'));
-app.use(urlencoded({ extended: false }));
 
 app.use(routes);
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send(err.message);
-});
+// Add production SPA work around
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000');
-});
+// if (process.env.NODE_ENV === 'production') {
+//   // Static folder
+//   app.use(express.static(__dirname + '/public/'))
+//
+//   // Handle SPA
+//   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+// }
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => (console.log(`Server started on port ${port}`)));
