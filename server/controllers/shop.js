310b2d55b8c28c26e7430146a36acc47a44c84a9
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-10-16 17:30:32 -0400

    Abstract try/catch code to helper to DRY-out controllers

--- server/controllers/shop.js
+++ server/controllers/shop.js
@@ -1,102 +1,71 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Product = require('../models/product');
 
 // @desc    Get all products
 // @route   GET /api/v1/products
 // @access  Public
-exports.getMany = async (req, res, next) => {
-  try {
-    const products = await Product.find();
-    res.status(200).json({ success: false, count: products.length, data: products });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.getMany = asyncHandler(async (req, res, next) => {
+  const products = await Product.find();
+  res.status(200).json({ success: false, count: products.length, data: products });
+});
 
 // @desc    Get single product
 // @route   GET /api/v1/products/:id
 // @access  Public
-exports.getOne = async (req, res, next) => {
-  try {
-    const product = await Product.findById(req.params.id);
+exports.getOne = asyncHandler(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
-    if (!product) {
-      return next(
-        new ErrorResponse(`Product not found with id of ${req.params.id}`, 404)
-      );
-    }
-    res.status(200).json({ success: true, data: product });
-  } catch (err) {
-    // res.status(400).json({ success: false });
-    next(err);
+  if (!product) {
+    return next(
+      new ErrorResponse(`Product not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ success: true, data: product });
+});
 
 // @desc    Create new product
 // @route   GET /api/v1/products
 // @access  Private
-exports.createOne = (req, res, next) => {
-  const title = req.body.title;
-  const price = req.body.price;
-  const description = req.body.description;
-  const imageUrl = req.body.imageUrl;
-  const product = new Product({
-    title: title,
-    price: price,
-    description: description,
-    imageUrl: imageUrl
-  });
-  product
-    .save()
-    .then(() => {
-      console.log('Created product');
-    })
-    .catch((err) => {
-      next(err);
-    });
+exports.createOne = asyncHandler(async (req, res, next) => {
+  const product = await Product.create(req.body);
 
-};
+  res.status(200).json({
+    success: true,
+    data: product
+  })
+});
 
 // @desc    Update product
 // @route   GET /api/v1/products/:id
 // @access  Private
-exports.updateOne = async (req, res, next) => {
-  try {
-    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+exports.updateOne = asyncHandler(async (req, res, next) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!product) {
-      return next(
-        new ErrorResponse(`Product not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: product });
-  } catch (err) {
-    next(err);
+  if (!product) {
+    return next(
+      new ErrorResponse(`Product not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ success: true, data: product });
+});
 
 // @desc    Delete a product
 // @route   GET /api/v1/products/:id
 // @access  Private
-exports.removeOne = async (req, res, next) => {
-  try {
-    const product = await Product.findByIdAndDelete(req.params.id);
+exports.removeOne = asyncHandler(async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
 
-    if (!product) {
-      return next(
-        new ErrorResponse(`Product not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: {} });
-  } catch (err) {
-    next(err);
+  if (!product) {
+    return next(
+      new ErrorResponse(`Product not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ success: true, data: {} });
+});
 
 
 
