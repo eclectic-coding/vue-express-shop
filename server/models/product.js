1209c45048cc389830ec77079657312241f260ff
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-10-16 19:10:57 -0400

    Add slugify for cleaner URL based on product name

--- server/models/product.js
+++ server/models/product.js
@@ -1,29 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
   title: {
     type: String,
-    required: true
+    required: [true, 'Please add a Product title']
   },
+  slug: String,
   price: {
-    type: String,
-    required: true
+    type: Number,
+    required: [true, 'Please add a Product price']
   },
   description: {
     type: String,
-    required: true
+    required: [true, 'Please add a Product description'],
+    maxlength: [500, 'Description can not be more than 500 characters']
   },
   imageUrl: {
     type: String,
     required: true
-  },
+  }
   // userId: {
   //   type: Schema.Types.ObjectId,
   //   ref: 'User',
   //   required: true
   // },
-})
+});
 
-module.exports = mongoose.model('Product', productSchema)
+// Create product slug from the title
+productSchema.pre('save', function (next) {
+  this.slug = slugify(this.title, { lower: true });
+  // console.log('Slugify ran', this.title);
+  next();
+
+});
+
+module.exports = mongoose.model('Product', productSchema);
